57d92b499e9cd97be0c47493c84a9cf1ef81a259
Nguyễn Ngô Huy <huy.nguyenngo13@hcmut.edu.vn>
2025-09-09 16:52:31 +0700

    Hot fix redirect

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+    </div>
+  )
+}
